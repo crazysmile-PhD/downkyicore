ae86d0897dd9b8ba4067e4ebb6b678eb92f4246b
赫鵓 <139638122+crazysmile-PhD@users.noreply.github.com>
2026-05-20 14:12:52 +0800

    Fix action versions and nullable annotations in targeted files

--- DownKyi.Core/BiliApi/Bangumi/Models/BangumiArea.cs
+++ DownKyi.Core/BiliApi/Bangumi/Models/BangumiArea.cs
@@ -1,10 +1,13 @@
-﻿using DownKyi.Core.BiliApi.Models;
+using DownKyi.Core.BiliApi.Models;
 using Newtonsoft.Json;
 
 namespace DownKyi.Core.BiliApi.Bangumi.Models;
 
 public class BangumiArea : BaseModel
 {
-    [JsonProperty("id")] public int Id { get; set; }
-    [JsonProperty("name")] public string Name { get; set; }
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; } = string.Empty;
 }
--- DownKyi.Core/BiliApi/Bangumi/Models/BangumiUpInfo.cs
+++ DownKyi.Core/BiliApi/Bangumi/Models/BangumiUpInfo.cs
@@ -1,19 +1,23 @@
-﻿using DownKyi.Core.BiliApi.Models;
+using DownKyi.Core.BiliApi.Models;
 using Newtonsoft.Json;
 
 namespace DownKyi.Core.BiliApi.Bangumi.Models;
 
 public class BangumiUpInfo : BaseModel
 {
-    [JsonProperty("avatar")] public string Avatar { get; set; }
+    [JsonProperty("avatar")]
+    public string Avatar { get; set; } = string.Empty;
 
     // follower
     // is_follow
-    [JsonProperty("mid")] public long Mid { get; set; }
+    [JsonProperty("mid")]
+    public long Mid { get; set; }
 
     // pendant
     // theme_type
-    [JsonProperty("uname")] public string Name { get; set; }
+    [JsonProperty("uname")]
+    public string Name { get; set; } = string.Empty;
+
     // verify_type
     // vip_status
     // vip_type
